9cdc696961afbb9f4ea0a02efd96c7093cd13374
anu225s <sreenathareddy12@gmail.com>
2023-12-29 23:43:40 +0530

    addition of two numbers

--- src/dem1/Dem1st.java
+++ src/dem1/Dem1st.java
@@ -3,7 +3,11 @@ package dem1;
 public class Dem1st {
 
 	public static void main(String[] args) {
-System.out.println("hi tes");
+System.out.println("hi we are committing 1stfile");
+int a =10;
+int b=21;
+int c= a+b;
+System.out.println(c);
 	}
 
 }
