1cae89664db4e37e33bcd73587c53ce468593439
anu225s <sreenathareddy12@gmail.com>
2024-02-05 07:27:12 +0530

    okay

--- src/dem1/Dem1st.java
+++ src/dem1/Dem1st.java
@@ -4,6 +4,8 @@ public class Dem1st {
 
 	public static void main(String[] args) {
 System.out.println("hi we are committing 1stfile");
+
+System.out.println("hi goodmorning");
 int a =10;
 int b=21;
 int c= a+b;
